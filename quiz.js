d8f4536bee178cc711e1b89ab37a274676c23e7c
morrryyyy <morrryyyy20@gmail.com>
2025-12-01 13:13:42 +0100

    create checkAnswer functin and rename quiz.js

---
+++ quiz.js
@@ -0,0 +1,12 @@
+function checkAnswer() {
+  const correctAnswer = "4";
+  const feedback = document.getElementById("feedback");
+  let userAnswer = document.querySelector("input[name='quiz']:checked").value;
+  if (userAnswer === correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
+}
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
